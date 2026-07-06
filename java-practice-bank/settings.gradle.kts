a1d9c6c8513373b65ca2ce7eb20011ba4e9aad29
xiaoneng1122333-collab <xiaoneng1122333@gmail.com>
2026-07-06 20:20:25 +0900

    fix(phase1): remove invalid buildSrc include (reserved name)

--- java-practice-bank/settings.gradle.kts
+++ java-practice-bank/settings.gradle.kts
@@ -2,7 +2,7 @@
 rootProject.name = "java-practice-bank"
 
 // Phase 1 で作成するモジュール
-include("buildSrc")
+// buildSrc は Gradle の予約名のため include() 不要 (自動認識される)
 include("common:common-domain")
 include("common:common-batch")
 include("common:common-mybatis")
