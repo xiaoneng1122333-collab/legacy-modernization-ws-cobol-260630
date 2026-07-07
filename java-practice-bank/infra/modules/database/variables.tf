7bfa649b38979b12082eed1b63adfae08afd822d
xiaoneng1122333-collab <xiaoneng1122333@gmail.com>
2026-07-07 16:15:44 +0900

    modify

--- java-practice-bank/infra/modules/database/variables.tf
+++ java-practice-bank/infra/modules/database/variables.tf
@@ -1,6 +1,19 @@
-variable "project_name"          { type = string }
-variable "db_password"           { type = string; sensitive = true }
-variable "db_security_group_id"  { type = string }
-variable "cache_security_group_id" { type = string }
-variable "subnet_ids"            { type = list(string) }
-variable "tags"                  { type = map(string) }
+variable "project_name" {
+  type = string
+}
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+variable "db_security_group_id" {
+  type = string
+}
+variable "cache_security_group_id" {
+  type = string
+}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "tags" {
+  type = map(string)
+}
--- java-practice-bank/infra/modules/ecs/main.tf
+++ java-practice-bank/infra/modules/ecs/main.tf
@@ -1,6 +1,9 @@
 resource "aws_ecs_cluster" "main" {
   name = "${var.project_name}-cluster"
-  setting { name = "containerInsights" value = "enabled" }
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
   tags = var.tags
 }
 
